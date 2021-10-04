52e764c69aff31b3395424d8dfe39771549d3bcd
cazar27 <carloszr27@gmail.com>
2021-10-05 00:51:37 +0200

    add pokemon with ngForm

--- bases-2/src/app/pokemon/main-page/main-page.component.ts
+++ bases-2/src/app/pokemon/main-page/main-page.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Pokemon {
+  name: string,
+  number: number
+}
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
@@ -7,7 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
-  constructor() { }
+  pokemonNew: Pokemon = {
+    name: '',
+    number: 0
+  }
+
+  pokemons: Pokemon[] = [
+    {
+      name: 'Bulbasur',
+      number: 1
+    }
+  ]
+
+  constructor() {
+    this.pokemons.push({name: "Charmander", number: 4});
+    this.pokemons.push({name: "Squirtle", number: 7});
+    this.pokemons.push({name: "Caterpie", number: 10});
+  }
+
+  agregar() {
+    if(this.pokemonNew.name.trim().length === 0 ) {
+      return;
+    }
+    this.pokemons.push(this.pokemonNew);
+    console.log(this.pokemonNew);
+    this.pokemonNew = {
+      name: '',
+      number: 0
+    }
+  }
+
+  changeName( evt: any ):void {
+    evt.target.value;
+  }
 
   ngOnInit(): void {
   }
