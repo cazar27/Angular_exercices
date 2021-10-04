ea89504e3804ff0066bc10294f7283a9726df033
cazar27 <carloszr27@gmail.com>
2021-10-05 01:10:35 +0200

    template ready of pokemon list

--- bases-2/src/app/pokemon/pokemon.module.ts
+++ bases-2/src/app/pokemon/pokemon.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { MainPageComponent } from './main-page/main-page.component';
+import { PokemonListComponent } from './main-page/pokemon-list/pokemon-list.component';
 
 @NgModule({
-  declarations: [MainPageComponent],
+  declarations: [MainPageComponent, PokemonListComponent],
   exports: [MainPageComponent],
   imports: [
     CommonModule,
