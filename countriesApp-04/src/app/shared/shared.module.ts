a6a5aafa5d8342a65243c70f62c4b6951631bb90
cazar27 <carloszr27@gmail.com>
2021-11-23 12:47:29 +0100

    change order imports share module

--- countriesApp-04/src/app/shared/shared.module.ts
+++ countriesApp-04/src/app/shared/shared.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
+
 import { NavComponent } from './nav/nav.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
