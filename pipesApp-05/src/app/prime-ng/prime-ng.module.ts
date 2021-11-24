114591d9d01362ecadd421979127a7aa037781aa
cazar27 <carloszr27@gmail.com>
2021-11-24 13:41:41 +0100

    test init menu

--- pipesApp-05/src/app/prime-ng/prime-ng.module.ts
+++ pipesApp-05/src/app/prime-ng/prime-ng.module.ts
@@ -4,15 +4,12 @@ import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { MenubarModule } from 'primeng/menubar';
 
-import { SharedModule } from '../shared/shared.module';
-
 @NgModule({
   declarations: [],
   exports: [
     ButtonModule,
     CardModule,
-    MenubarModule,
-    SharedModule
+    MenubarModule
   ],
 })
 export class PrimeNgModule { }
--- pipesApp-05/src/app/shared/shared.module.ts
+++ pipesApp-05/src/app/shared/shared.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
+import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 
 @NgModule({
   declarations: [
     MenuComponent
   ],
-  imports: [
-    CommonModule,
-  ],
   exports: [
     MenuComponent
-  ]
+  ],
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    PrimeNgModule
+  ],
 })
 export class SharedModule { }
