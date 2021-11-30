45916b3d0296a49627d934bbed87a35ca5447bff
cazar27 <carloszr27@gmail.com>
2021-11-30 01:10:18 +0100

    finish routing hero start with data server

--- heroApp-06/src/app/heros/heros.module.ts
+++ heroApp-06/src/app/heros/heros.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AddHeroComponent } from './pages/add-hero/add-hero.component';
 import { SearchHeroComponent } from './pages/search-hero/search-hero.component';
 import { HomeHeroComponent } from './pages/home-hero/home-hero.component';
 import { ListHeroComponent } from './pages/list-hero/list-hero.component';
 import { HeroRouting } from './hero-routing.module';
+import { MaterialModule } from '../material/material.module';
 @NgModule({
   declarations: [
     AddHeroComponent,
@@ -15,6 +17,8 @@ import { HeroRouting } from './hero-routing.module';
   ],
   imports: [
     CommonModule,
+    FlexLayoutModule,
+    MaterialModule,
     HeroRouting
   ]
 })
