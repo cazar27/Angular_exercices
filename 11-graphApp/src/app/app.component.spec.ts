78e71d8aea9e5acfb83ac9a5ab472211fdfc4972
cazar27 <carloszr27@gmail.com>
2021-12-08 05:42:25 +0100

    finish graphApp

--- 11-graphApp/src/app/app.component.spec.ts
+++ 11-graphApp/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'graphApp'`, () => {
+  it(`should have as title 'graficasApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('graphApp');
+    expect(app.title).toEqual('graficasApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('graphApp app is running!');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('graficasApp app is running!');
   });
 });
